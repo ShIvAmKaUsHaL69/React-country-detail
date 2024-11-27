154ff76f5c2a86bd5a8096abd280bb7bcf15de48
ShIvAmKaUsHaL <9720sk41@gmail.com>
2024-11-27 17:56:24 +0530

    add correct way of presenting dark/light mode using localstorage

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,27 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Header() {
+  const [isdark , setisdark] = useState(JSON.parse(localStorage.getItem('isdarkmode')))
+if(isdark){
+  document.body.classList.add('dark')
+}else{
+  document.body.classList.remove('dark')
+}
+
   return (
 <header className="header-container">
   <div className="header-content">
     <h2 className="title">
       <a href="/">Where in the world?</a>
     </h2>
-    <p className="theme" onClick={() => {document.body.classList.toggle('dark')
-      const mode = document.querySelector("#mode")
-      mode.classList.toggle('fa-moon')
-      mode.classList.toggle('fa-sun')
-      const theme = document.querySelector('.theme')
-      if(mode.classList.contains('fa-sun')){
-        theme.innerHTML= `<i class="fa-regular fa-sun" id="mode"></i>
-      &nbsp;&nbsp;Light Mode`
-      }else{
-        theme.innerHTML= `<i class="fa-regular fa-moon" id="mode"></i>
-      &nbsp;&nbsp;Dark Mode`
-      }
+    <p className="theme" onClick={() => {
+      setisdark(!isdark)
+      localStorage.setItem('isdarkmode' , !isdark)
     }}>
-      <i className="fa-regular fa-moon" id="mode"/>
-      &nbsp;&nbsp;Dark Mode
+      <i className={`fa-solid fa-${isdark ? 'sun' : 'moon'}`}/>
+      &nbsp;&nbsp;{isdark? 'Light' : 'Dark'} Mode
     </p>
   </div>
 </header>
